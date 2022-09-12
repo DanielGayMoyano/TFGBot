8915161d586d86e9b473e2170bef36c91f281e2b
DanielGayMoyano <106381548+DanielGayMoyano@users.noreply.github.com>
2022-09-12 12:25:49 +0200

    avanzado el comando help

--- eventos/guild/interactionCreate.js
+++ eventos/guild/interactionCreate.js
@@ -7,7 +7,33 @@ const {
 } = require("@discordjs/voice");
 const { VoiceConnection } = require("@discordjs/voice");
 const playlists = require("../../playlist/playlists.json");
+const { readdirSync } = require("fs");
 module.exports = (client, interaction, message, args) => {
+  if (interaction.customId === "playlists") {
+    createRadio(client, interaction, message);
+  }
+  if (interaction.customId === "categories") {
+    createMessageCategorie(client, interaction, message, args);
+  }
+  //console.log(interaction.customId);
+};
+function createMessageCategorie(client, interaction, message, args) {
+  const comandos = readdirSync(`./commands/${interaction.values[0]}`);
+  
+  let listaComandos = [];
+
+  comandos.forEach((c) => {
+    let categoria = {
+      "label": c.name,
+      "value": c.name,
+    };
+    listaComandos.push(categoria);
+  });
+console.log(listaComandos)
+  interaction.deferUpdate("");
+}
+
+function createRadio(client, interaction, message) {
   if (!interaction.member.voice?.channel)
     return interaction.reply(
       `❌**Tienes que estar en un canal de voz para ejecutar este comando!**`
@@ -20,28 +46,32 @@ module.exports = (client, interaction, message, args) => {
       client.distube.stop(interaction.guild.id);
     }
   } else {
-    let canal=setupRadioChannel.findOne({guildId: interaction.guild.id},
-      async(err,data)=>{
-        if(!data) return;
+    let canal = setupRadioChannel.findOne(
+      { guildId: interaction.guild.id },
+      async (err, data) => {
+        if (!data) return;
         let text = data.channelId.toString();
         let result = text.substring(2, text.length - 1);
         return result;
-      });
-    queue = new Queue(client.distube, interaction.member.voice, interaction.values[0], canal);
+      }
+    );
+    queue = new Queue(
+      client.distube,
+      interaction.member.voice,
+      interaction.values[0],
+      canal
+    );
   }
-  
-  client.distube
-    .play(interaction.member.voice?.channel, interaction.values[0], {
+
+  client.distube.play(
+    interaction.member.voice?.channel,
+    interaction.values[0],
+    {
       member: interaction.member,
       textChannel: interaction.channelId,
       message,
-    });
+    }
+  );
   interaction.deferUpdate(`Buena elección`);
-   
-    
-  //queue = client.distube.getQueue(interaction.guild); //.toggleAutoplay(interaction);
-  //let cola = client.distube.getQueue(interaction.guild.id);
-  //console.log(cola);
   queue.setRepeatMode(2);
-  console.log(queue);
-};
+}
